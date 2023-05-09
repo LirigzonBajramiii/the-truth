43c009b3f6169fe6e3ec10fcc1cddfba02d30dce
LirigzonBajrami <121976651+LirigzonBajrami@users.noreply.github.com>
2023-05-09 12:16:40 +0200

    api and database setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("test");
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Krijimi i nje express app
+const app = express();
+const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  // Listen for requests only if it's connected to database
+  app.listen(PORT, () => {
+    console.log(`Connected to db & Listening on port ${PORT}`);
+  });
+})
+.catch((error) => {
+  console.log(error);
+});
+
+
