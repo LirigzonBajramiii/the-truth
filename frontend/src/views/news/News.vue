7e167e64fad60e5b888d460a7043bd4995ec1f71
LirigzonBajrami <121976651+LirigzonBajrami@users.noreply.github.com>
2023-05-09 11:11:54 +0200

    Add code for structuring news in News page

--- frontend/src/views/news/News.vue
+++ frontend/src/views/news/News.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    News
+    <div class="latest-news">
+      <section class="latest">
+        Lajmi i fundit 
+      </section>
+      <section class="other-news">
+        Lajme tjera
+      </section>
+    </div>
+    <div class="in-focus">
+      <section class="in-focus-news">
+        In focus lajme
+      </section>
+      <section class="slider">
+        Slider
+      </section>
+    </div>
   </div>
 </template>
 
@@ -10,4 +25,43 @@
   };
 </script>
 
-<style scoped></style>
+<style scoped>
+.latest-news {
+  display: grid;
+  grid-template-columns: 6fr 3fr;
+  gap: 22px;
+  color: #fff;
+
+}
+
+.latest {
+  width: 100%;
+  height: 400px;
+  background-color: darkcyan;
+}
+
+.other-news {
+  width: 100%;
+  height: auto;
+  background-color: brown;
+}
+
+.in-focus {
+  display: grid;
+  grid-template-columns: 6fr 3fr;
+  gap: 22px;
+  color: #fff;
+  margin-top: 58px;
+}
+
+.in-focus-news {
+  width: 100%;
+  height: auto;
+  background-color: coral;
+}
+
+.slider {
+  background-color: darkgreen;
+}
+
+</style>
