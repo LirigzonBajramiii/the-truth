f0de6d7790e00e16978f62d2ff848f3c1be51cb1
LirigzonBajrami <121976651+LirigzonBajrami@users.noreply.github.com>
2023-05-20 18:36:20 +0200

    Login and register Views

--- frontend/src/views/users/LoginView.vue
+++ frontend/src/views/users/LoginView.vue
@@ -1,13 +1,87 @@
 <template>
   <div>
-    Login
+    <el-form
+      :model="user"
+      :rules="rules"
+      ref="user"
+      label-width="120px"
+      class="demo-ruleForm login-container"
+    >
+      <h3 class="login-title">Login</h3>
+      <!-- Email -->
+      <el-form-item label="Email" prop="email">
+        <el-input type="email" v-model="user.email"></el-input>
+      </el-form-item>
+
+      <!-- Password -->
+      <el-form-item label="Password" prop="password">
+        <el-input type="password" v-model="user.password"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('user')">Login</el-button>
+        <el-button @click="resetForm('user')">Reset</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
   name: "login-view",
+  data() {
+    return {
+      user: {
+        email: "",
+        password: "",
+      },
+      rules: {
+        email: [
+          {
+            required: true,
+            message: "Please enter your email",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "Please enter your password",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log(this.user);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.login-container {
+  max-width: 600px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  padding: 42px 32px;
+  margin: 90px auto;
+  border-radius: 9px;
+}
+
+.login-title {
+  text-align: center;
+  margin-bottom: 22px;
+}
+</style>
--- frontend/src/views/users/RegisterView.vue
+++ frontend/src/views/users/RegisterView.vue
@@ -1,13 +1,121 @@
 <template>
   <div>
-    Register
+    <el-form
+      :model="newUser"
+      :rules="rules"
+      ref="newUser"
+      label-width="120px"
+      class="demo-ruleForm register-container"
+    >
+      <h3 class="register-title">Register</h3>
+      <!-- First name -->
+      <el-form-item label="First name" prop="firstName">
+        <el-input v-model="newUser.firstName"></el-input>
+      </el-form-item>
+
+      <!-- Last Name -->
+      <el-form-item label="Last name" prop="lastName">
+        <el-input v-model="newUser.lastName"></el-input>
+      </el-form-item>
+
+      <!-- Email -->
+      <el-form-item label="Email" prop="email">
+        <el-input type="email" v-model="newUser.email"></el-input>
+      </el-form-item>
+
+      <!-- Password -->
+      <el-form-item label="Password" prop="password">
+        <el-input type="password" v-model="newUser.password"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('newUser')"
+          >Register</el-button
+        >
+        <el-button @click="resetForm('newUser')">Reset</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
   name: "register-view",
+  data() {
+    return {
+      newUser: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      },
+      rules: {
+        firstName: [
+          {
+            required: true,
+            message: "Please enter your first name",
+            trigger: "blur",
+          },
+          {
+            min: 3,
+            max: 5,
+            message: "Length should be 3 to 5",
+            trigger: "blur",
+          },
+        ],
+        lastName: [
+          {
+            required: true,
+            message: "Please enter your last name",
+            trigger: "blur",
+          },
+        ],
+        email: [
+          {
+            required: true,
+            message: "Please enter your email",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "Please enter your password",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log(this.newUser);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.register-container {
+  max-width: 600px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  padding: 42px 32px;
+  margin: 90px auto;
+  border-radius: 9px;
+}
+
+.register-title {
+  text-align: center;
+  margin-bottom: 22px;
+}
+</style>
