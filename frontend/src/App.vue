f10ffa54d76aed572f22b0c3c590f9bd014abb91
LirigzonBajrami <121976651+LirigzonBajrami@users.noreply.github.com>
2023-05-22 20:01:18 +0200

    handling user auth on reload

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,24 +3,38 @@
     <Header></Header>
 
     <!-- router-view shfaq contentin qe e permbane nje view  -->
-     <div class="container">
-        <router-view />   
-     </div>
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-import Header from '@/components/utilities/Header.vue'
+import Header from "@/components/utilities/Header.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { mapMutations } from "vuex";
 export default {
-  name: 'app-view',
+  name: "app-view",
   components: {
-    Header
-  }
-}
+    Header,
+  },
+  methods: {
+    ...mapMutations({
+      SET_USER: "users/SET_USER",
+    }),
+  },
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.SET_USER(user);
+      }
+    });
+  },
+};
 </script>
 
 <style>
-
 * {
   padding: 0;
   margin: 0;
@@ -36,6 +50,4 @@ body {
   margin: 0 auto;
   padding: 50px 0;
 }
-
-
 </style>
